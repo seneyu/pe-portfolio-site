00b6c56b6ae812636eba7ac900aabbe0eb0755a0
Lucas Kim <lucaskamadakim@gmail.com>
2025-06-20 19:42:04 -0700

    Revert back to not using Jinja to avoid conflicts later

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,6 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
-from jinja2 import Environment, PackageLoader, select_autoescape
-
-env = Environment(
-    loader=PackageLoader("app"),
-    autoescape=select_autoescape()
-)
 
 load_dotenv()
 app = Flask(__name__)
