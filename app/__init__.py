d127fb37b2d9b16fe6b6a6643509e1aebfd85826
Lucas Kim <lucaskamadakim@gmail.com>
2025-06-20 20:08:51 -0700

    Add hobbies for Lucas

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+env = Environment(
+    loader=PackageLoader("app"),
+    autoescape=select_autoescape()
+)
 
 load_dotenv()
 app = Flask(__name__)
@@ -9,3 +15,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/lucas/hobbies')
+def hobbies():
+    hobbies = [
+        {
+            "name": "Basketball",
+            "description": "I love to play basketball and watch it.",
+            "image_url": "/static/img/lucas-basketball.jpg"
+        },
+        {
+            "name": "Hackathons",
+            "description": "I love to hack and build things.",
+            "image_url": "/static/img/lucas-hackathon.jpeg"
+        },
+    ]
+    return env.get_template('hobbies.html').render(title="Hobbies", url=os.getenv("URL"), hobbies=hobbies, image_url="/static/img/lucas-picture.png")
