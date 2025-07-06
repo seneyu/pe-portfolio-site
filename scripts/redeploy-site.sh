1a3aa5a69a4a768e0aed5107c0d5d7c839a25b8b
Stephany Ho <shohk39@gmail.com>
2025-07-05 21:20:21 -0700

    add redeploy-site.sh

---
+++ scripts/redeploy-site.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+tmux kill-server
+
+cd mlh-portfolio-site
+git fetch && git reset origin/main --hard
+
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+
+tmux new-session -d -s myflask 'cd ~/mlh-portfolio-site && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
