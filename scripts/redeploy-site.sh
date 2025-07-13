5c5607f2cfde4730c61d564bd9651fa3f927379c
Lucas Kim <lucaskamadakim@gmail.com>
2025-07-12 22:44:17 -0400

    Update redeploy site script

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-tmux kill-server
-
 cd ~/mlh-portfolio-site
 git fetch
 git reset origin/main --hard
@@ -9,4 +7,5 @@ git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-tmux new-session -d -s site_session 'cd ~/mlh-portfolio-site && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
+systemctl daemon-reload
+systemctl restart myportfolio
